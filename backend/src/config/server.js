589715d47b2a3ba4acbd3fdfa31be38c1f9910a7
unknown <allan.juliato@gmail.com>
2018-03-31 01:25:34 -0300

    Ultimo Commit

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,12 +3,14 @@ const port = 3004
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const alllowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(alllowCors)
 
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}`)
 })
 
-123
+module.exports = server
