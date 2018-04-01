58967503f79f9b6709774ce4627ae0c00b09f80e
unknown <allan.juliato@gmail.com>
2018-04-01 19:16:54 -0300

    Ultimo Commit antes do Redux

--- backend/src/api/todo/todoServices.js
+++ backend/src/api/todo/todoServices.js
@@ -1,6 +1,6 @@
 const Todo = require('./todo')
 
-Todo.methods(['get', 'post', 'put', 'delete'])
+Todo.methods(['post', 'get', 'put', 'delete'])
 Todo.updateOptions({new: true, runValidators: true})
 
 module.exports = Todo
